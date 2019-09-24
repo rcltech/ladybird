04deaa046f889881af2b8a619a2b7ea33ad1aa28
utkarsh867 <utkarsh867@gmail.com>
2019-09-25 06:49:09 +0800

    Updates

---
+++ precache-manifest.c39e59ea194c2e35c8e205673e2472bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef439b07f8f1a1ab17a6769324274fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf959493c1b4a556edf",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2c65a845e40c5638235b",
+    "url": "/static/js/2.21b938c0.chunk.js"
+  },
+  {
+    "revision": "7bf959493c1b4a556edf",
+    "url": "/static/js/main.eb70c60f.chunk.js"
+  },
+  {
+    "revision": "b28d3f527ac5df1c9f51",
+    "url": "/static/js/runtime-main.248bcd6c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
