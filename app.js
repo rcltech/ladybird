e1344d29fbf671370d9057812e8d0337af7194ce
utkarsh867 <utkarsh867@gmail.com>
2019-10-09 22:52:32 +0800

    adds travis to run tests

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const path = require('path')
 
+app.get("/ping", (req, res) => {
+    res.send("OK")
+});
+
 app.use(express.static(path.join(__dirname, 'client','build')));
 app.get("/*", (req, res) => {
-    console.log(__dirname);
     res.sendFile(path.join(__dirname, 'client','build', 'index.html'))
 });
 
-const PORT = (process.env.PORT) || 3000;
-
-app.listen(PORT);
+module.exports = app;
