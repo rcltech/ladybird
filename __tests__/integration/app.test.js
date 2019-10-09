2921d4c9f6b3523a5d5d6306cd972393c8b00179
utkarsh867 <utkarsh867@gmail.com>
2019-10-10 01:34:49 +0800

    adds jwt encoding functionality

--- __tests__/integration/app.test.js
+++ __tests__/integration/app.test.js
@@ -1,24 +1,24 @@
-const app = require('../../app');
-const chai = require('chai');
-const request = require('supertest');
+const app = require("../../app");
+const chai = require("chai");
+const request = require("supertest");
 
 const expect = chai.expect;
 
 describe("API server", () => {
-    it('should return 200 status on ping', function (done) {
-        request(app)
-            .get("/ping")
-            .end((err, res) => {
-                expect(res.statusCode).to.equal(200);
-                done();
-            })
-    });
-    it('should return OK on ping', (done) => {
-        request(app)
-            .get("/ping")
-            .end((err, res) => {
-                expect(res.text).to.equal("OK");
-                done();
-            });
-    })
+  it("should return 200 status on ping", function(done) {
+    request(app)
+      .get("/ping")
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+  });
+  it("should return OK on ping", done => {
+    request(app)
+      .get("/ping")
+      .end((err, res) => {
+        expect(res.text).to.equal("OK");
+        done();
+      });
+  });
 });
