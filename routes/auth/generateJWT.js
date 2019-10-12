9a1afa71a7c9bc01fb8939b087a9afc187e51fb8
utkarsh867 <utkarsh867@gmail.com>
2019-10-12 17:40:50 +0800

    #12: Restructures to react-only app

--- routes/auth/generateJWT.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const generateJWT = jsonObject => jwt.sign(jsonObject, process.env.JWT_SECRET);
-
-module.exports = generateJWT;
