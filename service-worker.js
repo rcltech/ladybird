9c99a155cdf17c1d56881431ba966b23a50cedd9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-14 15:07:22 +0000

    Deploy rcltech/ladybird to github.com/rcltech/ladybird.git:gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5887d5924e81ce292aef57795c94d75a.js"
+  "/precache-manifest.1af31a289defb93a8d384d9dfb168038.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
