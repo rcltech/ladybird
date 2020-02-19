64b2de1684db163dc52afffdd700cd35922d27bc
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-19 16:10:46 +0000

    Deploy rcltech/ladybird to github.com/rcltech/ladybird.git:gh-pages

---
+++ precache-manifest.2669f6be02cf5b60a2adf9e74ef2d4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0395166ba1a578fe8fe09314227dc4bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a92cc87c49be1902ff8",
+    "url": "/static/js/2.c5bb6c10.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/static/js/2.c5bb6c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa96c29a8f97481fe64",
+    "url": "/static/js/main.b1773fcb.chunk.js"
+  },
+  {
+    "revision": "a4664ad21323dc96b9e3",
+    "url": "/static/js/runtime-main.5a657c87.js"
+  },
+  {
+    "revision": "83c070978ca8c25b115cc2b20c76c5d4",
+    "url": "/static/media/logo.83c07097.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.94db11c60957d68e9d565fe91fbb3ed2.js"
+  "/precache-manifest.2669f6be02cf5b60a2adf9e74ef2d4c6.js"
 );
 
 self.addEventListener('message', (event) => {
