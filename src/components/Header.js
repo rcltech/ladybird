18241d875027fb9a273e99e313aac988fbe156c9
seanchok <seanchok@gmail.com>
2020-04-22 12:01:04 +0100

    fix styling
    remove duplicate material-ui/styles package

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import logo from "../images/logo.png";
 import { Container } from "@material-ui/core";
-import Typography from "@material-ui/core/Typography";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const useStyles = makeStyles(theme => ({
