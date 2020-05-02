1683bb55e9f431d23440fcbf4ba69ff5f549cd46
seanchok <seanchok@gmail.com>
2020-05-02 16:35:51 +0100

    fix redirection bug

--- src/components/withAuthConfigApollo.js
+++ src/components/withAuthConfigApollo.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import qs from "query-string";
 
 export const withAuthConfigApollo = ChildComponent => {
   const clientID =
@@ -13,6 +14,18 @@ export const withAuthConfigApollo = ChildComponent => {
 
   return props => {
     const [googleUser, setGoogleUser] = useState();
+    const { location } = props;
+
+    if (
+      sessionStorage.getItem("redirectTo") === "" ||
+      sessionStorage.getItem("redirectTo") === null ||
+      qs.parse(location.search).redirectTo
+    ) {
+      sessionStorage.setItem(
+        "redirectTo",
+        qs.parse(location.search).redirectTo || ""
+      );
+    }
 
     const link = new HttpLink({
       uri: HOST_URL,
@@ -28,12 +41,20 @@ export const withAuthConfigApollo = ChildComponent => {
       link,
     });
 
+    let redirectUrl = "";
+    if (sessionStorage.getItem("redirectTo") !== "")
+      redirectUrl =
+        process.env.REACT_APP_ENV === "development"
+          ? `http://${sessionStorage.getItem("redirectTo")}`
+          : `https://${sessionStorage.getItem("redirectTo")}`;
+
     return (
       <ApolloProvider client={client}>
         <ChildComponent
           googleUser={googleUser}
           setGoogleUser={setGoogleUser}
           clientID={clientID}
+          redirectUrl={redirectUrl}
           {...props}
         />
       </ApolloProvider>
